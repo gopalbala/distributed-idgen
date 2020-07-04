f97580fe6d3a56521beb1bdb392a56c031dac297
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-04 21:41:43 +0530

    Added main class

---
+++ src/main/java/com/gb/didgen/DistributedIdDesignMain.java
@@ -0,0 +1,11 @@
+package com.gb.didgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedIdDesignMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedIdDesignMain.class);
+    }
+}
