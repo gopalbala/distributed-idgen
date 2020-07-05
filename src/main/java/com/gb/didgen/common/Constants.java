cb61eb1401c6adbeebfc3f8cec890fb149f4eb25
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-05 06:46:24 +0530

    added Constants.java

---
+++ src/main/java/com/gb/didgen/common/Constants.java
@@ -0,0 +1,8 @@
+package com.gb.didgen.common;
+
+public class Constants {
+    public static final int SIGN_BIT_LEN = 1;
+    public static final int EPOCH_BIT_LEN = 41;
+    public static final int NODE_ID_BIT_LEN = 10;
+    public static final int SEQUENCE_BIT_LEN = 12;
+}
