f291307947d221d7168af290b5b3dee7916c3e0a
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-05 08:44:58 +0530

    added exception to signature

--- src/main/java/com/gb/didgen/service/IdGenerator.java
+++ src/main/java/com/gb/didgen/service/IdGenerator.java
@@ -1,7 +1,8 @@
 package com.gb.didgen.service;
 
 import com.gb.didgen.exception.ClockMovedBackException;
+import com.gb.didgen.exception.NodeIdOutOfBoundException;
 
 public interface IdGenerator {
-    long generateId() throws ClockMovedBackException;
+    long generateId() throws ClockMovedBackException, NodeIdOutOfBoundException;
 }
