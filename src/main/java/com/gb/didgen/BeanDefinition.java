ddbd71561a533785168b1240d0b645cb6f70fb13
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-07-05 06:45:50 +0530

    create node value from mac address

---
+++ src/main/java/com/gb/didgen/BeanDefinition.java
@@ -0,0 +1,39 @@
+package com.gb.didgen;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.security.SecureRandom;
+import java.util.Enumeration;
+
+import static com.gb.didgen.common.Constants.NODE_ID_BIT_LEN;
+
+@Component
+public class BeanDefinition {
+    @Bean
+    public int generatingNodeId() {
+        int maxNodeVal = (int) Math.pow(2, NODE_ID_BIT_LEN);
+        int nodeId;
+        try {
+            StringBuilder sb = new StringBuilder();
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                byte[] mac = networkInterface.getHardwareAddress();
+                if (mac != null) {
+                    for (int i = 0; i < mac.length; i++) {
+                        sb.append(String.format("%02X", mac[i]));
+                    }
+                }
+            }
+            nodeId = sb.toString().hashCode();
+        } catch (SocketException ex) {
+            //in case of exception get a random number limited by max node size
+            nodeId = (int) (new SecureRandom().nextInt() % Math.pow(2, 10));
+        }
+        nodeId = nodeId & maxNodeVal;
+        return nodeId;
+    }
+}
